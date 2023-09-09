2462cde4a80963eed0c1bc19c4d49df27922d2ea
Diego <damunerat@eafit.edu.co>
2023-09-09 11:58:16 -0500

    agregando modelos order, customer, prenda

--- clothstore/store/admin.py
+++ clothstore/store/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Clothe
+from .models import Order
 # Register your models here.
-admin.site.register(Clothe)
+admin.site.register(Order)
--- clothstore/store/models.py
+++ clothstore/store/models.py
@@ -8,4 +8,19 @@ class Order(models.Model):
     prenda_id = models.IntegerField()
     order_date = models.DateField()
     payment_method = models.CharField(max_length=50)
-    
+
+class Prenda(models.Model):
+    prenda_id = models.AutoField(primary_key=True)
+    prenda_category = models.CharField(max_length=50)
+    prenda_price = models.IntegerField()
+    prenda_color = models.CharField(max_length=50)
+    prenda_size = models.CharField(max_length=50)
+    prenda_description = models.CharField(max_length=50)
+
+class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    customer_name = models.CharField(max_length=50)
+    customer_address = models.CharField(max_length=50)
+    customer_phone = models.CharField(max_length=50)
+    customer_email = models.CharField(max_length=50)
+    customer_payment_method = models.CharField(max_length=50)
