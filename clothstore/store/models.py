558b4305619d4da272bb3f05806aae235be252c6
Diego <damunerat@eafit.edu.co>
2023-09-09 11:53:00 -0500

    Modificando modelo

--- clothstore/store/models.py
+++ clothstore/store/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Clothe(models.Model):
-    id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=50)
-    tipo = models.CharField(max_length=50)
-    precio = models.CharField(max_length=20)
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    customer_id = models.IntegerField()
+    customer_name = models.CharField(max_length=50)
+    prenda_id = models.IntegerField()
+    order_date = models.DateField()
+    payment_method = models.CharField(max_length=50)
+    
