62ae24ce90ff80b1febf7810d8d7cfc963ed51aa
Diego <damunerat@eafit.edu.co>
2023-09-09 14:16:22 -0500

    Agregando plantillas esteticas

--- clothstore/store/urls.py
+++ clothstore/store/urls.py
@@ -3,4 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('nosotros/', AboutPageView.as_view(), name='nosotros'),
+    path('contacto/', ContactPageView.as_view(), name='contacto'),
+    path('blog/', BlogPageView.as_view(), name='blog'),
 ]
--- clothstore/store/views.py
+++ clothstore/store/views.py
@@ -4,4 +4,14 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class HomePageView(TemplateView):
-    template_name = 'home.html'
+    template_name = 'pages/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/nosotros.html'
+
+class ContactPageView(TemplateView):
+    template_name = 'pages/contacto.html'
+
+class BlogPageView(TemplateView):
+    template_name = 'pages/blog.html'
+
